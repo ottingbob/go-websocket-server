09eaa3607c43374e10f60cc878f8afc68da330e4
yob <otting.8@osu.edu>
2019-05-07 00:27:28 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+*** GO Websocket Example
+
+Simple websocket example.
+
+Plans to add broadcasting functionality connect to backend and multi channel support.
+
+Spin up a docker compose to host the app too.
---
+++ websockets.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to the browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+
+	fmt.Println("[Running on 127.0.0.1:8000]")
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.ListenAndServe(":8000", nil)
+}
